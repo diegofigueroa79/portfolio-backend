dea47f7a2c8494ab58c8eedc4fe42be82923b2d4
Diego Figueroa <diegofigueroa79@gmail.com>
2023-06-25 17:23:58 -0700

    add historical data endpoint

--- coins/urls.py
+++ coins/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 app_name = "coins"
 
 urlpatterns = [
-    path('coins/', views.coin_list, name='coin_list')
+    path('coins/', views.coin_list, name='coin_list'),
+    path('coins/<str:slug>/', views.coin_detail, name='coin_detail'),
+    path('coins/history/<str:symbol>/<str:period>/', views.coin_detail_historical, name='coin_detail_historical')
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
